58e05077fd8ada81aa4485612fc0bce1b74e57b5
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-03-09 20:57:40 +0800

    menambahkan input data

--- ProjectConditionStatement.cpp
+++ ProjectConditionStatement.cpp
@@ -2,3 +2,9 @@
 using namespace std;
 
 // Prosedur untuk memasukkan data
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
